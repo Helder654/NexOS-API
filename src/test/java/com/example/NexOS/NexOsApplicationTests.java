22cb40e391d65be84e8f1700f0b657de907eb132
Hélder Ferreira <helder654@gmail.com>
2026-08-26 20:05:59 -0300

    chore: configure PostgreSQL and environment variables

---
+++ src/test/java/com/example/NexOS/NexOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NexOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
